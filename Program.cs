d1c213f46da8f3bd195f398abc0fc1b160b730a7
brian_li <brian.li@sgs.com>
2025-03-04 15:42:47 +0800

    refactor: 重構應用程式啟動邏輯
    - 使用 `HostApplicationBuilder` 取代 `Host.CreateDefaultBuilder`。
    - 新增環境變數的讀取功能。
    - 增強配置設置以支持 JSON 配置檔。
    - 整合 Serilog 作為日誌機制。
    - 改進錯誤處理，確保日誌正確關閉。
    - 更新服務註冊以使用 `AddWindowsService()`。

--- Program.cs
+++ Program.cs
@@ -1,13 +1,57 @@
+﻿using Serilog;
+using SGS.OAD.Helper;
+
 namespace demoWindowsService;
 
 class Program
 {
     static Task Main(string[] args)
     {
-        return Host.CreateDefaultBuilder(args)
-            .UseWindowsService() // 設定為 Windows Service
-            .ConfigureServices(services => services.AddHostedService<Worker>())
-            .Build()
-            .RunAsync();
+        var builder = new HostApplicationBuilder(args);
+
+        // 取得環境變數
+        string environment = builder.Environment.EnvironmentName;
+        string appName = builder.Environment.ApplicationName;
+        string basePath = builder.Environment.ContentRootPath;
+
+        // 設定組態檔
+        builder.Configuration.SetBasePath(basePath)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true);
+
+        // 設定 Serilog
+        Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Configuration)
+                .Enrich.WithProperty("Application", appName) //應用程式名稱不寫於設定檔
+                .Enrich.WithProperty("Version", VersionHelper.CurrentVersion)
+                .CreateLogger();
+
+        Log.Information("{Application} 於 {EnvironmentName} 啟動", appName, environment);
+
+        try
+        {
+            // 清除預設的日誌提供者
+            builder.Logging.ClearProviders();
+            // 使用 Serilog 取代內建的日誌機制
+            builder.Logging.AddSerilog();
+
+            // 設定為 Windows Service
+            builder.Services.AddWindowsService();
+            // 添加 Worker 服務
+            builder.Services.AddHostedService<Worker>();
+
+            // 構建和運行應用
+            var host = builder.Build();
+            return host.RunAsync();
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "{Application} 啟動失敗", appName);
+            return Task.FromException(ex);
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
     }
 }
